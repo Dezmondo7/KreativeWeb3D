301886f8bb9acec055c2b8a387f5838e8173083a
Dale <dalewarby23@outlook.com>
2025-04-18 15:04:27 +0100

    added the showcase section

--- src/sections/Showcase.jsx
+++ src/sections/Showcase.jsx
@@ -1,41 +1,48 @@
 import React from 'react';
 import Curated from '../public/images/Curated.png';
-import '../index.css'
 
 const Showcase = () => {
     return (
         <div id="work" className="app-showcase">
             <div className="w-full">
                 <div className="showcaselayout">
-                    {/*left side */}
-                    <div className="first-project-wrapper">
-                        <div className="image-wrapper">
-                            <img src={Curated} alt="Curated" />
-                        </div>
-                        <div className="text-content">
-                            <h2>Museum App created to navigate through time and ancient artifacts. user friendly app called Curated</h2>
-                        <p className="text-white-50 md:text-xl"> An app built wit HTML, CSS, Tailwind & Javascript
+                {/*left */}
+                <div className="first-project-wrapper">
+                    <div className="image-wrapper">
+                        <img src={Curated} alt="Curated Museum App" />
+                    </div>
+                    <div className="text-content space-y-5 mt-5 text-lg md:text-xl lg:text-2xl font-[1000] mt-5 ">
+                        <h1>Museum App. user friendly app called Curated making it easy to explore ancient artifcats</h1>
+                        <p className="text-white-50 md:text-xl font-light " >
+                            Raect App built with HTML, CSS & Tailwind.
                         </p>
-                        <div className="project-list-wrapper overflow hidden">
-                    <div className="project">
-                        <div className="image-wrapper bg-[#FFEFDB]">
-                            <img src={Curated} />
-                        </div>
-                        <p>HELLLOOO</p>
                     </div>
                 </div>
-                        
-                        </div>
-                    </div>  
-                    {/* Right */}
-                    <div className="project-list-wrapper overflow hidden">
-                    <div className="project">
-                        <div className="image-wrapper bg-[#FFEFDB]">
-                            <img src={Curated} />
-                        </div>
-                        <p>HELLLOOO</p>
+
+            {/* Right */}
+            <div className="project-list-wrapper overflow-hidden" >
+                <div className="project">
+                    <div className="image-wrapper bg-[#ffefdb]">
+                        <img src={Curated} alt="Curated" />
                     </div>
+                    <h2>ANother app here</h2>
                 </div>
+
+                <div className="project">
+                    <div className="image-wrapper bg-[#ffe7eb]">
+                        <img src={Curated} alt="Curated" />
+                    </div>
+                    <h2>ANother app here</h2>
+                </div>
+            </div>
+
+
+            </div>
+        </div>
+        </div>
+
+        
+        
 
                 
 
@@ -45,9 +52,7 @@ const Showcase = () => {
 
                    
                     
-                 </div>
-            </div>
-        </div>
+            
     )
 }
 

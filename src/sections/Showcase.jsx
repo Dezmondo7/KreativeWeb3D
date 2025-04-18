cfa3e7276c2a7a6e4269dba6a1273682445f4c86
Dale <dalewarby23@outlook.com>
2025-04-18 17:45:05 +0100

    added the animation to the showcase section using gsap

--- src/sections/Showcase.jsx
+++ src/sections/Showcase.jsx
@@ -1,13 +1,52 @@
 import React from 'react';
+import { useRef } from 'react';
 import Curated from '../public/images/Curated.png';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'; 
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger); {/* lets the page know a scrolling function is happening */}
 
 const Showcase = () => {
+
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+    
+   
+    useGSAP(() => {
+        const projects = [project1Ref.current, project2Ref.current, project3Ref.current]; 
+
+        projects.forEach(( card, index ) => {
+            gsap.fromTo(
+                card, 
+                {
+                    y: 50, opacity: 0
+                },
+                {
+                    y: 0 ,
+                    opacity: 1,
+                    duration: 1,
+                    delay: 0.3 * (index + 1),
+                    scrollTrigger: {
+                        trigger: card, 
+                        start: 'top bottom-=100'
+                    }
+                }
+            )
+        })
+        gsap.fromTo(sectionRef.current, { opacity: 0 }, { opacity: 1, duration: 1.5 })
+
+    }, []);
+
+
     return (
-        <div id="work" className="app-showcase">
+        <section id="work" ref={sectionRef} about className="app-showcase">
             <div className="w-full">
                 <div className="showcaselayout">
                 {/*left */}
-                <div className="first-project-wrapper">
+                <div className="first-project-wrapper" ref={project1Ref}>
                     <div className="image-wrapper">
                         <img src={Curated} alt="Curated Museum App" />
                     </div>
@@ -21,14 +60,14 @@ const Showcase = () => {
 
             {/* Right */}
             <div className="project-list-wrapper overflow-hidden" >
-                <div className="project">
+                <div className="project" ref={project2Ref}>
                     <div className="image-wrapper bg-[#ffefdb]">
                         <img src={Curated} alt="Curated" />
                     </div>
                     <h2>ANother app here</h2>
                 </div>
 
-                <div className="project">
+                <div className="project" ref={project3Ref}>
                     <div className="image-wrapper bg-[#ffe7eb]">
                         <img src={Curated} alt="Curated" />
                     </div>
@@ -39,7 +78,7 @@ const Showcase = () => {
 
             </div>
         </div>
-        </div>
+        </section>
 
         
         

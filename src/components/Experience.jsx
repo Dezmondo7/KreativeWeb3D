441488d487c6b8a24d7c6906839903919007f3a2
Dale <dalewarby23@outlook.com>
2025-04-26 20:53:04 +0100

    finished the timeline animation using gsap

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import TitleHeader from './TitleHeader'
-import { expCards } from '../constants/index';
+import { expCards } from '../constants/index'
 import GlowCard from '../components/GlowCard';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Experience = () => {
+
+    useGSAP(() => {
+        gsap.utils.toArray('.timeline-card').forEach((card) => {
+
+            gsap.from(card, {
+                xPercent: -100,
+                opacity: 0,
+                transformOrigin: 'left left',
+                duration: 1, 
+                ease: 'Power2.inOut',
+                scrollTrigger: {
+                    trigger: card,
+                    start: 'top 80%',
+                }
+            })
+    })
+        gsap.to('.timeline', {
+            transformOrigin: 'bottom bottom',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '.timeline',
+                start: 'top center',
+                end: '70% center',
+                onUpdate: (self) => {
+                    gsap.to('.timeline', {
+                        scaleY: 1 - self.progress,
+                    })
+                }
+            },
+            
+        })
+
+    }, []);
+
     return (
         <section id="experience w-full md:mt-40 mt-20 section-padding xl:px-0">
             <div className="w-full h-full md:px-20 px-5">

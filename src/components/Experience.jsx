70c2129d6ee58289aed0dea9dd1c25b8b18233cd
Dale <dalewarby23@outlook.com>
2025-07-29 21:46:14 +0100

    added the title to the experience section which will become about us

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -63,7 +63,7 @@ const Experience = () => {
     return (
         <section id="experience w-full md:mt-40 mt-20 section-padding xl:px-0">
             <div className="w-full h-full md:px-20 px-5">
-                <TitleHeader title="Professional Work Experience" sub="My Career Overview" />
+                <TitleHeader title="Reakt Web Design" sub="About Us" />
 
                 <div className="relative">
                     <div className="relative z-50 xl:space-y-32 space-y-10 mt-40" >
